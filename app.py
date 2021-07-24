4b3863cc0797d9bdf81affbc4316ef1efca7bd9c
s2kstd9 <s2kstd9@outlook.kr>
2021-07-24 14:09:07 +0900

    master

---
+++ app.py
@@ -0,0 +1,15 @@
+﻿from flask import Flask, make_response, jsonify, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return 'Hello World! 바보멍충이 ggggg'
+
+@app.route('/main')
+def show_main_page():
+    html = "<html><body>니가바보</body></html>"
+    return html
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
